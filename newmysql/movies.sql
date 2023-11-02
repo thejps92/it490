b102f10be2b68d971b0e3824dede06d0cd11c42c
root <root@it490.2.myguest.virtualbox.org>
2023-11-01 23:09:17 -0400

    movie table

---
+++ newmysql/movies.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    Year INT,
+    Director VARCHAR(255),
+	Genre VARCHAR(255),
+	Cast_member VARCHAR(255)
+);
+
