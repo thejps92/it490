39383b13dde074a9194a63de849a812103dfce5a
trevor <trevor.trevor.com>
2023-11-02 19:22:57 -0400

    Added search_bar file to assist with rest of functionality

--- apache/homepage.php
+++ apache/homepage.php
@@ -1,4 +1,6 @@
-
+<?php
+include('search_bar.php');
+?>
 
 <!DOCTYPE html>
 <html>
@@ -8,6 +10,34 @@
 <body>
 	<header>
 		<h1>Homepage</h1>
+<br>
+<form method="post" action="">
+	<input type="text" name="search_query" placeholder="Search for a movie by title">
+	<input type="submit" value="Search">
+</form>
+
+<?php
+
+if(isset($_POST['search_query'])){
+	$search_query = $_POST['search_query'];
+	$movies = searchMovies($conn, $search_query);
+
+	if(!empty($movies)){
+		echo "<h2>Search Results:</h2>";
+		echo "<ul>";
+		foreach($movies as $movie){
+			echo "<li>$movie</li>";
+		}
+		echo "</ul>";
+	}else{
+		echo "No matching movies found.";
+	}
+}
+
+?>
+
+<br>
+<br>
 	<nav>
 	<ul>
 		<li><a href="homepage.php">Home</a></li>
