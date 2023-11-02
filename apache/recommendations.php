2017969f8d3639dad51eb7c030de785ca4c98576
trevor <trevor.trevor.com>
2023-11-01 21:56:34 -0400

    Added new files: bookmark, friends, homepage, profile, and recommendations

---
+++ apache/recommendations.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+</head>
+<body>
+	<header>
+		<h1>Homepage</h1>
+	<nav>
+	<ul>
+		<li><a href="homepage.php">Home</a></li>
+		<li><a href="recomendations.php">Recommendations</a></li>
+		<li><a href="profile.php">Profile</a></li>
+		<li><a href="bookmark.php">Bookmarks</a></li>
+		<li><a href="friends.php">Friends</a></li>
+	</ul>
+	</nav>
+	</header>
+
+	<main>
+	<section>
+		<h2>Home</h2>
+		<p>This is the homepage.</p>
+	</section>
+	</main>
+
+	<footer>
+	   <p>&copy; <?php echo date("Y"); ?> My Basic Website</p>
+	</footer>
+</body>
+</html>
