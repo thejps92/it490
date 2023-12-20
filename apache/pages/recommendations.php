ba9c5905c06fe959d3cfed1cda690c07c2904273
Trevor Toerock <98994172+GaleTrevor@users.noreply.github.com>
2023-12-19 23:27:15 -0500

    Update recommendations.php
    Added bootstrap stuff.

--- apache/pages/recommendations.php
+++ apache/pages/recommendations.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
-if (isset($_SESSION['user_id'])) {
+if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['fav_genre'], $_SESSION['movies'])) {
 	$user_id = $_SESSION['user_id'];
+	$username = $_SESSION['username'];
+	$fav_genre = $_SESSION['fav_genre'];
 	$movies = $_SESSION['movies'];
 } else {
     header('Location: signin.php');
@@ -13,40 +15,43 @@ if (isset($_SESSION['user_id'])) {
 <html>
 <head>
 	<title>Recommendations</title>
+	
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	
 </head>
 <body>
 	<header>
-		<h1>Recommendations</h1>
-	<nav>
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="recommendations.php">Recommendations</a></li>
-		<li><a href="profile.php">Profile</a></li>
-		<li><a href="bookmarks.php">Bookmarks</a></li>
-		<li><a href="friends.php">Friends</a></li>
-	</ul>
+		<nav class="navbar navbar-expand-lg navbar-light bg-info">
+	<div class="container-fluid">
+		<a class="navbar-brand text-white">490Central</a>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link text-white" href="recommendations.php">Recommendations</a>
+				<a class="nav-item nav-link text-white" href="profile.php">Profile</a>
+				<a class="nav-item nav-link text-white" href="bookmarks.php">Bookmarks</a>
+				<a class="nav-item nav-link text-white" href="friends.php">Friends</a>
+			</div>
+			</div>
+	</div>
 	</nav>
 	</header>
 
 	<main>
 	<section>
+	<br>
 		<h2>Recommendations</h2>
-		<?php
-		if (!empty($movies)) {
-			echo '<table>';
-			echo '<tr><th>Title</th><th>Year</th><th>Genre</th></tr>';
-			foreach ($movies as $movie) {
-				echo '<tr>';
-				echo '<td>' . $movie['title'] . '</td>';
-				echo '<td>' . $movie['year'] . '</td>';
-				echo '<td>' . $movie['genre'] . '</td>';
-				echo '</tr>';
-			}
-			echo '</table>';
-		} else {
-			echo '<p>No recommended movies available.</p>';
-		}
-		?>
+		<ul>
+            <?php
+            foreach ($movies as $movie) {
+                echo '<li>' . $movie['title'] . ' - ' . $movie['year'] . ' - ' . $movie['genre'] . '</li>';
+            }
+            ?>
+        </ul>
 	</section>
 	</main>
 
@@ -55,5 +60,10 @@ if (isset($_SESSION['user_id'])) {
                 <input type="submit" name="signout" value="Sign Out">
         </form>	   
 	</footer>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
 </body>
 </html>
