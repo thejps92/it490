dd2f01c9637276ce12d94cd7649f563658ba7576
Trevor Toerock <98994172+GaleTrevor@users.noreply.github.com>
2023-12-19 23:45:36 -0500

    Update profile.php
    Updated with bootstrap menu and emails.

--- apache/pages/profile.php
+++ apache/pages/profile.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['fav_genre'])) {
+if (isset($_SESSION['user_id'])) {
 	$user_id = $_SESSION['user_id'];
 	$username = $_SESSION['username'];
+	$email = $_SESSION['email'];
 	$fav_genre = $_SESSION['fav_genre'];
 } else {
     header('Location: signin.php');
@@ -42,12 +43,12 @@ if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['fav_genre'])) 
 
 	<main>
 	<br>
-	
 	<section>
 		<h2>Profile</h2>
-		<p>User ID: <?php echo $user_id; ?></p>
-		<p>Username: <?php echo $username; ?></p>
-		<p>Favorite Genre: <?php echo $fav_genre; ?></p>
+		<p>User ID: <strong><?php echo $user_id; ?></strong></p>
+		<p>Username: <strong><?php echo $username; ?></strong></p>
+		<p>Email: <strong><?php echo $email; ?></strong></p>
+		<p>Favorite Genre: <strong><?php echo $fav_genre; ?></strong></p>
 	</section>
 	</main>
 
@@ -56,5 +57,10 @@ if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['fav_genre'])) 
                 <input type="submit" name="signout" value="Sign Out">
         </form>
 	</footer>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
 </body>
 </html>
