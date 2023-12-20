91405bdbf1cb2d6341d0faefbca09c9a48430025
Trevor Toerock <98994172+GaleTrevor@users.noreply.github.com>
2023-12-19 23:40:59 -0500

    Update bookmarks.php
    Updated bookmarks for php and email changes.

--- apache/pages/bookmarks.php
+++ apache/pages/bookmarks.php
@@ -13,20 +13,33 @@ if (isset($_SESSION['user_id'])) {
 <html>
 <head>
 	<title>Bookmarks</title>
+	
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	
 </head>
 <body>
 	<header>
-		<h1>Bookmarks</h1>
-	<nav>
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="recommendations.php">Recommendations</a></li>
-		<li><a href="profile.php">Profile</a></li>
-		<li><a href="bookmarks.php">Bookmarks</a></li>
-		<li><a href="friends.php">Friends</a></li>
-	</ul>
+		<nav class="navbar navbar-expand-lg navbar-light bg-info">
+	<div class="container-fluid">
+		<a class="navbar-brand text-white">490Central</a>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link text-white" href="recommendations.php">Recommendations</a>
+				<a class="nav-item nav-link text-white" href="profile.php">Profile</a>
+				<a class="nav-item nav-link text-white" href="bookmarks.php">Bookmarks</a>
+				<a class="nav-item nav-link text-white" href="friends.php">Friends</a>
+			</div>
+			</div>
+	</div>
 	</nav>
 	</header>
+
+<br>
 
 	<main>
 	<section>
@@ -50,7 +63,7 @@ if (isset($_SESSION['user_id'])) {
 		?>
 	</section>
 	</main>
-
+	
 	<script>
     document.addEventListener('click', function (event) {
         if (event.target && event.target.className === 'remove') {
@@ -90,5 +103,10 @@ if (isset($_SESSION['user_id'])) {
                 <input type="submit" name="signout" value="Sign Out">
         </form> 
 	</footer>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
 </body>
 </html>
