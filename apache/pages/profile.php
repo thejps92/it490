228c7086b15f0abe7257dfff2c5ab80fbe6bec41
Trevor Toerock <98994172+GaleTrevor@users.noreply.github.com>
2023-12-19 23:31:51 -0500

    Update profile.php
    Added bootstrap menu.

--- apache/pages/profile.php
+++ apache/pages/profile.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-if (isset($_SESSION['user_id'])) {
+if (isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['fav_genre'])) {
 	$user_id = $_SESSION['user_id'];
 	$username = $_SESSION['username'];
-	$email = $_SESSION['email'];
 	$fav_genre = $_SESSION['fav_genre'];
 } else {
     header('Location: signin.php');
@@ -15,28 +14,40 @@ if (isset($_SESSION['user_id'])) {
 <html>
 <head>
 	<title>Profile</title>
+	
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	
 </head>
 <body>
 	<header>
-		<h1>Profile</h1>
-	<nav>
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="recommendations.php">Recommendations</a></li>
-		<li><a href="profile.php">Profile</a></li>
-		<li><a href="bookmarks.php">Bookmarks</a></li>
-		<li><a href="friends.php">Friends</a></li>
-	</ul>
+		<nav class="navbar navbar-expand-lg navbar-light bg-info">
+	<div class="container-fluid">
+		<a class="navbar-brand text-white">490Central</a>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link text-white" href="index.php">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link text-white" href="recommendations.php">Recommendations</a>
+				<a class="nav-item nav-link text-white" href="profile.php">Profile</a>
+				<a class="nav-item nav-link text-white" href="bookmarks.php">Bookmarks</a>
+				<a class="nav-item nav-link text-white" href="friends.php">Friends</a>
+			</div>
+			</div>
+	</div>
 	</nav>
 	</header>
 
 	<main>
+	<br>
+	
 	<section>
 		<h2>Profile</h2>
-		<p>User ID: <strong><?php echo $user_id; ?></strong></p>
-		<p>Username: <strong><?php echo $username; ?></strong></p>
-		<p>Email: <strong><?php echo $email; ?></strong></p>
-		<p>Favorite Genre: <strong><?php echo $fav_genre; ?></strong></p>
+		<p>User ID: <?php echo $user_id; ?></p>
+		<p>Username: <?php echo $username; ?></p>
+		<p>Favorite Genre: <?php echo $fav_genre; ?></p>
 	</section>
 	</main>
 
